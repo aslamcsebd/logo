dbc86d98a1ce79daa9bdc92af4619fb9109b3eb1
aslamcsebd <aslamcsebd@gmail.com>
2022-10-10 18:26:55 +0600

    Link build

---
+++ include/navbar.php
@@ -0,0 +1,48 @@
+<div class="topNav mx-4 p-1">
+   <span>
+      <i class="fa-regular fa-envelope p-2"></i>
+      logomillions@gmail.com
+   </span>
+   <span class="float-end">
+      Order by Whatsapp : <i class="fa-brands fa-whatsapp"></i> 01876638414
+   </span>
+</div>
+
+<nav class="navbar navbar-expand-lg bg-light">   
+   <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+         <img src="../images/logo.jpeg" height="50" width="80">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+               <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+               </a>
+               <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+               </ul>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link disabled">Disabled</a>
+            </li>
+         </ul>
+         <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+         </form>
+      </div>
+   </div>
+</nav>
