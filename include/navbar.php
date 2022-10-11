e8bb8a31b70c6b6ff639c391eb4812b9ff97268f
aslamcsebd <aslamcsebd@gmail.com>
2022-10-11 18:22:59 +0600

    Navbar design complete

--- include/navbar.php
+++ include/navbar.php
@@ -5,48 +5,40 @@
          logomillions@gmail.com
       </span>
    </div>
-   <div class="col-md-4">
+   <div class="col-md-4 second">
       <span class="rounded-5 px-2 py-1 my-1 me-4 float-end">
          Order by Whatsapp : <i class="fa-brands fa-whatsapp"></i> 01876638414
       </span>
    </div>
 </div>
 
-<nav class="navbar navbar-expand-lg bg-light">   
+<nav class="navbar navbar-expand-lg p-0">   
    <div class="container-fluid">
       <a class="navbar-brand" href="#">
-         <img src="../images/logo.jpeg" height="50" width="80">
+         <img class="rounded-2" src="images/logo.jpeg" height="40" width="80">
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+         <ul class="navbar-nav m-auto mb-2 mb-lg-0">
             <li class="nav-item">
-               <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
+               <a class="btn btn-outline-primary" href="#">Logo design</a>
+            </li>    
             <li class="nav-item">
-               <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-               </a>
-               <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-               </ul>
+               <a class="btn btn-outline-secondary" href="#">Service</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link disabled">Disabled</a>
-            </li>
+               <a class="btn btn-outline-success" href="#">About us</a>
+            </li>    
          </ul>
-         <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-         </form>
+
+        
+         <a class="btn btn-outline-success shopping" href="#">
+            <i class="fa-solid fa-cart-shopping fs-5"></i>
+            Order now
+         </a>
+        
       </div>
    </div>
 </nav>
